99f3a6a4588ee99bfe4205b9d5c3bf2c5fda3cab
Itay Cohen <itaycs2407@gmail.com>
2023-07-25 11:14:51 +0300

    Update assn3_tests.py - fail

--- tests/assn3_tests.py
+++ tests/assn3_tests.py
@@ -21,7 +21,7 @@ def test_dish_creation():
     for dish_name in dishes_list:
         response = requests.post(base_url + dishes_resource, json={"name": dish_name})
         dish_id = int(response.text)
-        assert response.status_code == 201
+        assert response.status_code == 800
         assert dish_id not in dishes_col
         dishes_col[dish_name] = dish_id
 
@@ -30,54 +30,9 @@ def test_get_dishes_by_id():
     orange_id = dishes_col[orange]
     response = requests.get(f'{base_url}{dishes_resource}/{orange_id}')
     sodium = response.json()['sodium']
-    assert response.status_code == 200
+    assert response.status_code == 800
     assert 0.9 <= sodium <= 1.1
 
-
-def test_get_dishes():
-    response = requests.get(f'{base_url}{dishes_resource}')
-    dishes_json = response.json()
-    count = 0
-    for dish_json in dishes_json:
-        count += 1
-        assert is_json(dish_json) is True
-    assert count == 3
-    assert response.status_code == 200
-
-
-def test_post_non_existent_dish():
-    response = requests.post(base_url + dishes_resource, json={"name": 'blah'})
-    response_status = response.status_code
-    assert response_status == 400 or response_status == 404 or response_status == 422
-    assert response.json() == -3
-
-
-def test_post_dish_already_in_server():
-    response = requests.post(base_url + dishes_resource, json={"name": orange})
-    response_status = response.status_code
-    assert response_status == 400 or response_status == 404 or response_status == 422
-    assert response.json() == -2
-
-
-def test_meal_creation():
-    request_json = {'name': delicious, 'appetizer': dishes_col[orange],
-                    'main': dishes_col[spaghetti], 'dessert': dishes_col[apple_pie]}
-    response = requests.post(base_url + meals_resource, json=request_json)
-    meal_col[delicious] = response.json()
-    assert response.json() > 0
-    assert response.status_code == 201
-
-
-def test_get_meals():
-    response = requests.get(f'{base_url}{meals_resource}')
-    dish = response.json()[str(meal_col[delicious])]
-    # for dish in response.json():
-    assert 400 <= dish['cal'] <= 500
-    assert len(response.json()) == 1
-    assert response.status_code == 200
-
-
-def test_post_meal_already_in_server():
     request_json = {'name': delicious, 'appetizer': dishes_col[orange],
                     'main': dishes_col[spaghetti], 'dessert': dishes_col[apple_pie]}
     response = requests.post(base_url + meals_resource, json=request_json)
